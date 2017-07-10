6f09fd120a3dcd065bc4a7074c0626d228efb7dd
amiinous <aminfotovat@gmail.com>
2017-07-10 10:08:54 +0430

    Initial Commit

---
+++ GoMoviez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoMoviez
+//
+//  Created by Amin Fotovat on 7/10/17.
+//  Copyright © 2017 Aminous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
